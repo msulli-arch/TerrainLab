9734a7c404eb9816b1e2be91008c32c42a62950b
msulli-arch <74615437+msulli-arch@users.noreply.github.com>
2020-11-17 12:52:39 -0500

    standard map fix

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ function createMap() {
     //Varibles for attribution box on the map.
     var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' + ' <a href="http://mapbox.com">Mapbox</a>'; //You can add you name to the variable if you want to add yourself to the credits.
     
-    //Variable for storing your Mapbox API Token
-    var apitoken = 'pk.eyJ1IjoibXN1bGxpdmFuOTQiLCJhIjoiY2toa3c3YmlqMHU4bDJ4cGYyaDB3bmc1dyJ9.zmWrvNbiUWQfNyOjSPMkKg'; //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+	var apitoken = 'pk.eyJ1IjoibXN1bGxpdmFuOTQiLCJhIjoiY2toa3c3YmlqMHU4bDJ4cGYyaDB3bmc1dyJ9.zmWrvNbiUWQfNyOjSPMkKg';
 	
-	var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}'; //URL used for Stanard MaxBox Styles
+	var mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}'; //URL used for Stanard MaxBox Styles
 	
     //URL used for Custom MapBox Style
     var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}';
@@ -15,18 +14,18 @@ function createMap() {
     var customeBasemap = L.tileLayer(mbStyleUrl, {id: 'msullivan94/ckhkwolyr013x1amyl15v36ri', token: apitoken,  attribution: mbAttr});
     
 	//For MabBox Standard Basemaps
-    var standardBasemap   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    var standardBasemap = L.tileLayer(mbUrl, {id: 'msullivan94/ckhm8tw5z0poq19o8bvu4fo39', token: apitoken, attribution: mbAttr});
 	
     //create the map*/
     var map = L.map('map', {
-        center: [37.216916,-108.46726],//Coordinated to center the map
+        center: [37.216916, -108.46726],//Coordinated to center the map
         zoom: 11, //zoom level
         layers: customeBasemap //basemap
 	});
-	    //create the basemap control layer*/
+	//create the basemap control layer*/
     var baseLayers = {
 		"Standard": standardBasemap,
-        "Custom": customeBasemap
+        "DEM": customeBasemap
     };
     
     L.control.layers(baseLayers).addTo(map);
